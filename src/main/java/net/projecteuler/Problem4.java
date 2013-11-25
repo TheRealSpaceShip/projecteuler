6b2f514b66958816e9804ae5275956afd26b8b5c
TheRealSpaceShip <lysak.igor@gmail.com>
2013-11-25 22:55:13 +0200

    Prepare project

---
+++ src/main/java/net/projecteuler/Problem4.java
@@ -0,0 +1,20 @@
+package net.projecteuler;
+
+/**
+ * <B>Задача:</B>
+ * <p/>
+ * Число-палиндром с обеих сторон (справа и слева) читается одинаково. Самое большое число-палиндром, полученное произведением двух двухзначных чисел – 9009 = 91 × 99.
+ * <p/>
+ * Найдите самый большой палиндром, полученный произведением двух трёхзначных чисел.
+ * <p/>
+ * <B>Оригинал:</B>
+ * <p/>
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * <p/>
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+public class Problem4 {
+    public static void main(String[] args) {
+
+    }
+}
