54adee98dd7afcb411eeff2954d30a26b1c82124
Igor Lysak <lysak.igor@gmail.com>
2013-11-26 15:07:27 +0200

    Solve problem number 4

--- src/main/java/net/projecteuler/Problem4.java
+++ src/main/java/net/projecteuler/Problem4.java
@@ -1,5 +1,9 @@
 package net.projecteuler;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * <B>Задача:</B>
  * <p/>
@@ -15,6 +19,36 @@ package net.projecteuler;
  */
 public class Problem4 {
     public static void main(String[] args) {
+        Set<Integer> palindromes = new HashSet<>();
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                int result = i * j;
+                if (isPalindrome(result)) {
+                    System.out.println(result + " = " + i + " * " + j);
+                    palindromes.add(result);
+                }
+            }
+        }
+        Iterator<Integer> iterator = palindromes.iterator();
+        int max = 0;
+        while (iterator.hasNext()) {
+            int val = iterator.next();
+            if (val > max) {
+                max = val;
+            }
+        }
+        System.out.println(max);
+    }
 
+    private static String revert(String str) {
+        return str.length() > 1 ? str.substring(str.length() - 1) + revert(str.substring(0, str.length() - 1)) : str;
+    }
+
+    private static boolean isPalindrome(int n) {
+        return isPalindrome(String.valueOf(n));
+    }
+
+    private static boolean isPalindrome(String n) {
+        return n.substring(0, n.length() / 2).equals(revert(n.substring(n.length() / 2 + (n.length() % 2 == 0 ? 0 : 1))));
     }
 }
